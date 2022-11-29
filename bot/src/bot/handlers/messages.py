a0418f9af6ee59b258f845e22c0ea37a7d7f19f4
alcibiadescleinias <ivan.cheprasov@phystech.edu>
2022-11-29 20:10:26 +0300

    do not spam

--- bot/src/bot/handlers/messages.py
+++ bot/src/bot/handlers/messages.py
@@ -4,7 +4,7 @@ from bot.misc import dp
 from bot.utils import is_chat_remembered_handler_decorator
 
 
-@dp.message_handler()
+@dp.message_handler(regexp='(phd|doctor|dog|аспирант)')
 @is_chat_remembered_handler_decorator
 async def echo(message: types.Message):
     await message.answer(message.text)
