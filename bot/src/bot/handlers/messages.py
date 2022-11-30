7950ed2e9030ff7d39f4c228e567f64a07a60dac
alcibiadescleinias <ivan.cheprasov@phystech.edu>
2022-12-01 00:26:11 +0300

    fixup! fix list in env and add handler

--- bot/src/bot/handlers/messages.py
+++ bot/src/bot/handlers/messages.py
@@ -3,18 +3,18 @@ import logging
 from aiogram import types
 
 from bot.misc import dp
-from bot.utils import is_chat_remembered_handler_decorator
+from bot.utils import is_groupchat_remembered_handler_decorator
 
 logger = logging.getLogger(__name__)
 
 
 @dp.message_handler(regexp='(phd|doctor|dog|аспирант)')
-@is_chat_remembered_handler_decorator
+@is_groupchat_remembered_handler_decorator
 async def echo(message: types.Message):
     await message.answer(message.text)
 
 
 @dp.message_handler()
-@is_chat_remembered_handler_decorator
+@is_groupchat_remembered_handler_decorator
 async def big_brother_logging_u(message: types.Message):
     logger.info(message)
