5304598702c32e9a134b4e030dd870247ee3a012
alcibiadescleinias <ivan.cheprasov@phystech.edu>
2022-12-01 00:28:49 +0300

    noqa for enum comparison

--- bot/src/bot/utils.py
+++ bot/src/bot/utils.py
@@ -7,7 +7,7 @@ def is_groupchat_remembered_handler_decorator(func):
     """In order to find group chats where bot already exists."""
 
     async def wrapper(message: types.Message):
-        if not isinstance(message.content_type, types.ChatType.PRIVATE):
+        if not message.content_type == types.ChatType.PRIVATE:  # noqa
             await bot_chats_storage.set_chat(message.chat.id)
         return await func(message)
 
