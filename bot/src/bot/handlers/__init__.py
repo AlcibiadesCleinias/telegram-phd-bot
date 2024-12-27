d623484028b138fb1244847748da5f4105d08c6d
AlcibiadesCleinias <ivan.cheprasov@phystech.edu>
2024-12-27 20:40:48 +0100

    activate perplexity token add

--- bot/src/bot/handlers/__init__.py
+++ bot/src/bot/handlers/__init__.py
@@ -3,6 +3,7 @@ from .commands import help  # noqa
 from .commands import cancel  # noqa
 from .commands import chat_id  # noqa
 from .commands.ai import openai_contributor_token  # noqa
+from .commands.ai import perplexity_contributor_token  # noqa
 from .commands.ai import switch_discussion_mode  # noqa
 from .commands.superadmin import broadcast_message  # noqa
 from .commands.superadmin import stats  # noqa
